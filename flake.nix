1c6bb9191ed445d123a5d04a56df1f8cf97af8e0
Phosu Parsons <38191182+phossil@users.noreply.github.com>
2024-03-17 14:20:31 -0400

    remove flake-utils

--- flake.nix
+++ flake.nix
@@ -2,22 +2,44 @@
   description = "flake for testing miscellaneous derivations";
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-    flake-utils.url = github:numtide/flake-utils/v1.0.0;
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      lib = nixpkgs.lib;
-
-      # check the packages directory to see
-      # what is available
-      all-packages = import ./packages { inherit pkgs lib; };
     in
     {
       # these are yet to be added to nixpkgs
-      packages.${system} = flake-utils.lib.flattenTree all-packages;
+      packages.${system} = with pkgs; {
+        xcode = callPackage ./packages/libs/xcode { };
+        terminal = callPackage ./packages/terminal { };
+        price = callPackage ./packages/price { };
+        step_sync = callPackage ./packages/stepsync { };
+        themes-gtk = callPackage ./packages/plugins/themes-gtk { };
+        # broken
+        #cenon = callPackage ./packages/cenon { };
+        help_viewer = callPackage ./packages/helpviewer { };
+        ftp = callPackage ./packages/ftp { };
+        graphos = callPackage ./packages/graphos { };
+        # broken
+        #code_editor = callPackage ./packages/codeeditor { };
+        # broken
+        #steptalk = callPackage ./packages/steptalk { };
+        text_edit = callPackage ./packages/textedit { };
+        # broken
+        #corebase = callPackage ./packages/libs/corebase { };
+        dbuskit = callPackage ./packages/libs/dbuskit { };
+        # broken
+        #opal = callPackage ./packages/libs/opal { };
+        simplewebkit = callPackage ./packages/libs/simplewebkit { };
+        # i deleted my poorly made mirror
+        #vespucci = callPackage ./packages/vespucci {
+        #  simplewebkit = self.simplewebkit;
+        #};
+        # broken
+        themes-win_ux_theme = callPackage ./packages/plugins/themes-winuxtheme { };
+      };
 
       # make the flake look pretty :)
       formatter.${system} = pkgs.nixpkgs-fmt;
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
-# phossil's testing flake
+# GNUstep Apps
 
-if you use this you are either a friend, impatient, or crazy
+have a look at the [officially listed examples](https://gnustep.github.io/experience/examples.html) and [non-GNU's GAP](https://www.nongnu.org/gap/index.html) for GNUstep apps
+
+![:OsanaSmile:](https://cdn.discordapp.com/emojis/916810941281239040.webp)
